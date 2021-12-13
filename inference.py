aee0d4e4ea3ada0ae8e5ae9fc9e29b8c1c293185
최재강 <chl8273@naver.com>
2021-12-13 14:49:56 +0900

    Update inference.py

--- inference.py
+++ inference.py
@@ -94,4 +94,6 @@ def perform(batch_size):
 
 if __name__ == "__main__":
     for batch_size in batch_list:
+        print(batch_size)
+        print(type(batch_size)
         print(perform(batch_size))
